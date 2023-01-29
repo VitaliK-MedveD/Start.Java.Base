8ecfcec14c9e83a8d7391f5b269290e6eccbd044
VitaliK-MedveD <vitalik.medved@gmail.com>
2023-01-30 02:41:25 +0300

    task4.Sum

---
+++ src/Task4/Sum/Main.java
@@ -0,0 +1,17 @@
+package Task4.Sum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author VitaliK.MedveD
+ * Задача: Найдите сумму и произведение цифр числа, введенного с клавиатуры.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Sum sum = new Sum();
+        sum.run();
+    }
+}
---
+++ src/Task4/Sum/Sum.java
@@ -0,0 +1,40 @@
+package Task4.Sum;
+
+import java.util.Scanner;
+
+public class Sum {
+
+    public static int length(int n) {
+        int i;
+        for (i = 0; n != 0; i++) {
+            n /= 10;
+        }
+        return i;
+    }
+
+    public void run () {
+
+        int sum = 0;
+        int mult = 1;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите целое число: ");
+        int number = in.nextInt();
+        int[] num = new int[length(number)];
+
+        for (int i = 0; number != 0; i++) {
+            int a = (int) (number % 10);
+            number /= 10;
+            num[i] = a;
+        }
+        for (int i = 0; i < num.length; i++) {
+            sum = sum + num[i];
+        }
+        System.out.println("Сумма цифр = " + sum);
+
+        for (int i = 0; i < num.length; i++) {
+            mult = mult * num[i];
+        }
+        System.out.println("Произведение цифр = " + mult);
+    }
+}
